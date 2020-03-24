660937de589eadda79586cfbf12c19dcbfd713b8
Quang <quangntn0302@gmail.com>
2020-03-24 18:29:19 +0700

    change layout from folder public/ to folder public/template. Create folder partials, partials/nav. Divide part of header of index page. Test success

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
+// npm modules and node core module
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
 
+// setup
 const app = express()
+const publicDirectory = path.join(__dirname, '../public')
+const viewDirectory = path.join(__dirname, '../public/template')
+const partialsDirectory = path.join(__dirname, '../public/template/partials')
+const partialsNavbarDirectory = path.join(__dirname, '../public/template/partials/navbar')
 
+// middleware
+app.use(express.static(publicDirectory))
+hbs.registerPartials(partialsDirectory)
+hbs.registerPartials(partialsNavbarDirectory)
+
+// setup express
+app.set('view engine', 'hbs')
+app.set('views', viewDirectory)
+
+// get method
 app.get('/', (request, response) => {
-  response.send('start server success')
+  response.render('index')
 })
 
 app.listen(process.env.PORT || 3300)
